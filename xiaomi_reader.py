9d4dc03ab92b02b81ebbe94368523c78447cf1ec
david.dudas <david.dudas@outlook.com>
2021-03-05 22:53:34 +0100

    use firebase to upload data

--- xiaomi_reader.py
+++ xiaomi_reader.py
@@ -5,9 +5,13 @@ from LYWSDCGQ_handler import LYWSDCGQ_reader
 from SimpleLogger import SimpleLogger
 
 import time
-import subprocess
 import os
 import threading
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+from datetime import datetime
 
 def watchdog():
     watchdogLogger = SimpleLogger(verbose = True, loggerName = f"Watchdog")
@@ -44,6 +48,20 @@ if __name__ == "__main__":
     sensor3 = LYWSD03MMC_reader('A4:C1:38:D2:7A:00', "filament")
     sensor4 = LYWSDCGQ_reader('58:2D:34:38:3C:E2', "bathroom")
 
+    # Default values
+    kitchen_temp  = 0
+    kitchen_hum   = 0
+    kitchen_bat   = 0
+    bathroom_temp = 0
+    bathroom_hum  = 0
+    bathroom_bat  = 0
+    outside_temp  = 0
+    outside_hum   = 0
+    outside_bat   = 0
+    filament_temp = 0
+    filament_hum  = 0
+    filament_bat  = 0
+
     maxRetryCounter = 3
 
     retryCounter = 0
@@ -51,13 +69,16 @@ if __name__ == "__main__":
         ret = sensor1.read_data()
         if ret is not None:
             logger.log(f"Data from {sensor1.name} ({sensor1.mac}): {ret}")
-            sensor1JSON=f'{sensor1.name}-temperature={ret["temperature"]}&{sensor1.name}-humidity={ret["humidity"]}&{sensor1.name}-battery={ret["battery"]}'
+            #sensor1JSON=f'{sensor1.name}-temperature={ret["temperature"]}&{sensor1.name}-humidity={ret["humidity"]}&{sensor1.name}-battery={ret["battery"]}'
+            kitchen_temp = ret["temperature"]
+            kitchen_hum  = ret["humidity"]
+            kitchen_bat  = ret["battery"]
             break
         else:
             logger.log(f"Empty data from {sensor1.name} ({sensor1.mac}), retrying...", messageType = "WARN")
             retryCounter+=1
             if retryCounter > maxRetryCounter:
-                sensor1JSON=f'{sensor1.name}-temperature=0&{sensor1.name}-humidity=0&{sensor1.name}-battery=0'
+                #sensor1JSON=f'{sensor1.name}-temperature=0&{sensor1.name}-humidity=0&{sensor1.name}-battery=0'
                 logger.log(f"Couldn't connect to the sensor {maxRetryCounter} times to {sensor1.name} ({sensor1.mac})!", messageType = "ERROR")
                 break
 
@@ -67,13 +88,16 @@ if __name__ == "__main__":
         ret = sensor2.read_data()
         if ret is not None:
             logger.log(f"Data from {sensor2.name} ({sensor2.mac}): {ret}")
-            sensor2JSON=f'{sensor2.name}-temperature={ret["temperature"]}&{sensor2.name}-humidity={ret["humidity"]}&{sensor2.name}-battery={ret["battery"]}'
+            #sensor2JSON=f'{sensor2.name}-temperature={ret["temperature"]}&{sensor2.name}-humidity={ret["humidity"]}&{sensor2.name}-battery={ret["battery"]}'
+            outside_temp = ret["temperature"]
+            outside_hum  = ret["humidity"]
+            outside_bat  = ret["battery"]
             break
         else:
             logger.log(f"Empty data from {sensor2.name} ({sensor2.mac}), retrying...", messageType = "WARN")
             retryCounter+=1
             if retryCounter > maxRetryCounter:
-                sensor2JSON=f'{sensor2.name}-temperature=0&{sensor2.name}-humidity=0&{sensor2.name}-battery=0'
+                #sensor2JSON=f'{sensor2.name}-temperature=0&{sensor2.name}-humidity=0&{sensor2.name}-battery=0'
                 logger.log(f"Couldn't connect to the sensor {maxRetryCounter} times to {sensor2.name} ({sensor2.mac})!", messageType = "ERROR")
                 break
 
@@ -82,13 +106,16 @@ if __name__ == "__main__":
         ret = sensor3.read_data()
         if ret is not None:
             logger.log(f"Data from {sensor3.name} ({sensor3.mac}): {ret}")
-            sensor3JSON=f'{sensor3.name}-temperature={ret["temperature"]}&{sensor3.name}-humidity={ret["humidity"]}&{sensor3.name}-battery={ret["battery"]}'
+            #sensor3JSON=f'{sensor3.name}-temperature={ret["temperature"]}&{sensor3.name}-humidity={ret["humidity"]}&{sensor3.name}-battery={ret["battery"]}'
+            filament_temp = ret["temperature"]
+            filament_hum  = ret["humidity"]
+            filament_bat  = ret["battery"]
             break
         else:
             logger.log(f"Empty data from {sensor3.name} ({sensor3.mac}), retrying...", messageType = "WARN")
             retryCounter+=1
             if retryCounter > maxRetryCounter:
-                sensor3JSON=f'{sensor3.name}-temperature=0&{sensor3.name}-humidity=0&{sensor3.name}-battery=0'
+                #sensor3JSON=f'{sensor3.name}-temperature=0&{sensor3.name}-humidity=0&{sensor3.name}-battery=0'
                 logger.log(f"Couldn't connect to the sensor {maxRetryCounter} times to {sensor3.name} ({sensor3.mac})!", messageType = "ERROR")
                 break
 
@@ -97,19 +124,37 @@ if __name__ == "__main__":
         ret = sensor4.read_data()
         if ret is not None:
             logger.log(f"Data from {sensor4.name} ({sensor4.mac}): {ret}")
-            sensor4JSON=f'{sensor4.name}-temperature={ret["temperature"]}&{sensor4.name}-humidity={ret["humidity"]}&{sensor4.name}-battery={ret["battery"]}'
+            #sensor4JSON=f'{sensor4.name}-temperature={ret["temperature"]}&{sensor4.name}-humidity={ret["humidity"]}&{sensor4.name}-battery={ret["battery"]}'
+            bathroom_temp = ret["temperature"]
+            bathroom_hum  = ret["humidity"]
+            bathroom_bat  = ret["battery"]
             break
         else:
             logger.log(f"Empty data from {sensor4.name} ({sensor4.mac}), retrying...", messageType = "WARN")
             retryCounter+=1
             if retryCounter > maxRetryCounter:
-                sensor4JSON=f'{sensor4.name}-temperature=0&{sensor4.name}-humidity=0&{sensor4.name}-battery=0'
+                #sensor4JSON=f'{sensor4.name}-temperature=0&{sensor4.name}-humidity=0&{sensor4.name}-battery=0'
                 logger.log(f"Couldn't connect to the sensor {maxRetryCounter} times to {sensor4.name} ({sensor4.mac})!", messageType = "ERROR")
                 break
 
-    upload_data = f"{sensor1JSON}&{sensor2JSON}&{sensor3JSON}&{sensor4JSON}"
+    #upload_data = f"{sensor1JSON}&{sensor2JSON}&{sensor3JSON}&{sensor4JSON}"
 
-    subprocess.call(["bash", "./send_data.bash", upload_data])
+    # Connect to Firebase Firestore
+    cred = credentials.Certificate("home-sensors-credentials.json")
+    firebase_admin.initialize_app(cred)
+
+    firestore_db = firestore.client()
+    collection = firestore_db.collection('home_sensors_v1')
+
+    #example timestamp: "20201204-122331"
+    timestampFormat ='%Y%m%d-%H%M%S'
+
+    collection.add({'date': datetime.now(), 'timestamp': datetime.now().strftime(timestampFormat), 
+                    'kitchen_temp': kitchen_temp, 'kitchen_hum': kitchen_hum, 'kitchen_bat': kitchen_bat,
+                    'bathroom_temp': bathroom_temp, 'bathroom_hum': bathroom_hum, 'bathroom_bat': bathroom_bat,
+                    'outside_temp': outside_temp, 'outside_hum': outside_hum, 'outside_bat': outside_bat,
+                    'filament_temp': filament_temp, 'filament_hum': filament_hum, 'filament_bat': filament_bat
+    })
 
     # Gracefully stop watchdog
     shutdownFlag.set()
